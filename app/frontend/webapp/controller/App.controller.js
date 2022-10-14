68481d4a678babb4aed6d8af58ce79cfa429b6e4
Bartosz Kloc <atlf75800@gmail.com>
2022-10-14 13:28:41 +0200

    removed unnecessary files from App view and App controller

--- app/frontend/webapp/controller/App.controller.js
+++ app/frontend/webapp/controller/App.controller.js
@@ -1,21 +1,13 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("bksoft.frontend.controller.App", {
-        onInit() {
-        },
-        toggleThemeChange: function () {
-          if (this.getView().byId("themeSwitch").getState()) {
-            sap.ui.getCore().applyTheme("sap_fiori_3_dark");
-          } else {
-            sap.ui.getCore().applyTheme("sap_fiori_3");
-          }
-      },
-      });
-    }
-  );
-  
+  [
+    "sap/ui/core/mvc/Controller"
+  ],
+  function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("bksoft.frontend.controller.App", {
+      onInit() {
+      }
+    });
+  }
+);
--- app/frontend/webapp/controller/Reservation.controller.js
+++ app/frontend/webapp/controller/Reservation.controller.js
@@ -1,14 +1,89 @@
-sap.ui.define([
-    'sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'sap/ui/core/format/DateFormat', 'sap/ui/core/library'
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/unified/DateRange', 'sap/m/MessageToast', 'sap/ui/core/format/DateFormat', 'sap/ui/core/library', 'sap/ui/core/routing/History'],
+    function (Controller, DateRange, MessageToast, DateFormat, coreLibrary, History) {
         "use strict";
 
+        var CalendarType = coreLibrary.CalendarType;
+
         return Controller.extend("bksoft.frontend.controller.Reservation", {
+            oFormatYyyymmdd: null,
+
             onInit: function () {
+                this.oFormatYyyymmdd = DateFormat.getInstance({ pattern: "yyyy-MM-dd", calendarType: CalendarType.Gregorian });
+            },
+
+            handleCalendarSelect: function (oEvent) {
+                var oCalendar = oEvent.getSource();
+                this._updateText(oCalendar.getSelectedDates()[0]);
+            },
+
+            _updateText: function (oSelectedDates) {
+                var oSelectedDateFrom = this.byId("selectedDateFrom"),
+                    oSelectedDateTo = this.byId("selectedDateTo"),
+                    oDate;
+
+                if (oSelectedDates) {
+                    oDate = oSelectedDates.getStartDate();
+                    if (oDate) {
+                        oSelectedDateFrom.setText(this.oFormatYyyymmdd.format(oDate));
+                    } else {
+                        oSelectedDateTo.setText("No Date Selected");
+                    }
+                    oDate = oSelectedDates.getEndDate();
+                    if (oDate) {
+                        oSelectedDateTo.setText(this.oFormatYyyymmdd.format(oDate));
+                    } else {
+                        oSelectedDateTo.setText("No Date Selected");
+                    }
+                } else {
+                    oSelectedDateFrom.setText("No Date Selected");
+                    oSelectedDateTo.setText("No Date Selected");
+                }
+            },
+
+            handleSelectThisWeek: function () {
+                this._selectWeekInterval(6);
+            },
+
+            handleSelectWorkWeek: function () {
+                this._selectWeekInterval(4);
+            },
+
+            handleWeekNumberSelect: function (oEvent) {
+                var oDateRange = oEvent.getParameter("weekDays"),
+                    iWeekNumber = oEvent.getParameter("weekNumber");
+
+                if (iWeekNumber % 5 === 0) {
+                    oEvent.preventDefault();
+                    MessageToast.show("You are not allowed to select this calendar week!");
+                } else {
+                    this._updateText(oDateRange);
+                }
+            },
+
+            _selectWeekInterval: function (iDays) {
+                var oCurrent = new Date(), // get current date
+                    iWeekStart = oCurrent.getDate() - oCurrent.getDay() + 1,
+                    iWeekEnd = iWeekStart + iDays, // end day is the first day + 6
+                    oMonday = new Date(oCurrent.setDate(iWeekStart)),
+                    oSunday = new Date(oCurrent.setDate(iWeekEnd)),
+                    oCalendar = this.byId("calendar");
+
+                oCalendar.removeAllSelectedDates();
+                oCalendar.addSelectedDate(new DateRange({ startDate: oMonday, endDate: oSunday }));
+
+                this._updateText(oCalendar.getSelectedDates()[0]);
+            },
+            onNavBack: function () {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("MainView", {}, true);
+                }
             }
         });
+
     });
