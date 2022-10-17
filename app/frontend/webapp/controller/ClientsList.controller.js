db7f81e2ad63f44510acaa2b18d77ca3398d32da
Bartosz Kloc <atlf75800@gmail.com>
2022-10-17 13:24:55 +0200

    working input list in ClientList view

--- app/frontend/webapp/controller/ClientsList.controller.js
+++ app/frontend/webapp/controller/ClientsList.controller.js
@@ -1,21 +1,34 @@
-sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/m/MessageToast', 'sap/ui/core/routing/History'],
-    function (Controller, coreLibrary, History) {
-        "use strict";
-
-        return Controller.extend("bksoft.frontend.controller.ClientsList", {
-            onInit: function () {
-            },
-            onNavBack: function () {
-                var oHistory = History.getInstance();
-                var sPreviousHash = oHistory.getPreviousHash();
-
-                if (sPreviousHash !== undefined) {
-                    window.history.go(-1);
-                } else {
-                    var oRouter = this.getOwnerComponent().getRouter();
-                    oRouter.navTo("MainView", {}, true);
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("bksoft.frontend.controller.ClientsList", {
+        onInit: function () {
+            // set data model on view
+            var oDataInput = {
+                client: {
+                    firstName: "",
+                    secondName: "",
+                    phoneNum: "",
+                    email: ""
                 }
-            }
-        });
-
+            };
+            var oModel = new JSONModel(oDataInput);
+            this.getView().setModel(oModel);
+        },
+        onShowHello: function () {
+            var sClientFirstName = this.getView().getModel().getProperty("/client/firstName");
+            var sClientSecondName = this.getView().getModel().getProperty("/client/secondName");
+            var sClientPhoneNum = this.getView().getModel().getProperty("/client/phoneNum");
+            var sClientEmail = this.getView().getModel().getProperty("/client/email");
+            // show message
+            MessageToast.show("client added");
+            // console.log(sClientFirstName);
+            // console.log(sClientSecondName);
+            // console.log(sClientPhoneNum);
+            // console.log(sClientEmail);
+        }
     });
+});
