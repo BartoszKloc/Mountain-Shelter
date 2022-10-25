2f62b421b6f459ef9240d12e9aff1c9e6294a63b
Bartosz Kloc <atlf75800@gmail.com>
2022-10-25 09:54:34 +0200

    rebuilded data model in ClientsList, now it is taken without ajax, added dialog for delete client button

--- app/frontend/webapp/controller/ClientsList.controller.js
+++ app/frontend/webapp/controller/ClientsList.controller.js
@@ -4,46 +4,49 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/routing/History",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function (Controller, MessageToast, JSONModel, History, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "sap/m/Text"
+], function (Controller, MessageToast, JSONModel, History, Filter, FilterOperator, Dialog, Button, Text) {
     "use strict";
     return Controller.extend("bksoft.frontend.controller.ClientsList", {
         onInit: function () {
             // set data model on view
-            var oDataInput = {
-                client: {
-                    firstName: "",
-                    secondName: "",
-                    phoneNum: "",
-                    email: ""
-                }
-            };
-            var oModel = new JSONModel(oDataInput);
-            this.getView().setModel(oModel);
+            // var oDataInput = {
+            //     client: {
+            //         firstName: "",
+            //         secondName: "",
+            //         phoneNum: "",
+            //         email: ""
+            //     }
+            // };
+            // var oModel = new JSONModel(oDataInput);
+            // this.getView().setModel(oModel);
 
-            const View = this.getView();
+            // const View = this.getView();
 
-            async function test2(View) {
-                const res = await $.ajax({
-                    method: "GET",
-                    url: "/catalog/Clients",
-                    dataType: "JSON"
-                }).done((data) => {
-                    console.log(data.value)
-                    return data.value;
-                })
-                var oModelClients = new JSONModel;
-                oModelClients.setData(res);
-                View.setModel(oModelClients, 'clientsList');
-            }
-            test2(View);
+            // async function test2(View) {
+            //     const res = await $.ajax({
+            //         method: "GET",
+            //         url: "/catalog/Clients",
+            //         dataType: "JSON"
+            //     }).done((data) => {
+            //         console.log(data.value)
+            //         return data.value;
+            //     })
+            //     var oModelClients = new JSONModel;
+            //     oModelClients.setData(res);
+            //     View.setModel(oModelClients, 'clientsList');
+            // }
+            // test2(View);
 
         },
         onSubmit: function () {
-            var sClientFirstName = this.getView().getModel().getProperty("/client/firstName");
-            var sClientSecondName = this.getView().getModel().getProperty("/client/secondName");
-            var sClientPhoneNum = this.getView().getModel().getProperty("/client/phoneNum");
-            var sClientEmail = this.getView().getModel().getProperty("/client/email");
+            // var sClientFirstName = this.getView().getModel().getProperty("/client/firstName");
+            // var sClientSecondName = this.getView().getModel().getProperty("/client/secondName");
+            // var sClientPhoneNum = this.getView().getModel().getProperty("/client/phoneNum");
+            // var sClientEmail = this.getView().getModel().getProperty("/client/email");
 
             if ((sClientFirstName != "") && (sClientSecondName != "") && (sClientPhoneNum != "") && (sClientEmail != "")) {
                 MessageToast.show("client added");
@@ -74,20 +77,41 @@ sap.ui.define([
                 oRouter.navTo("MainView", {}, true);
             }
         },
-        // deleteClient: function (oEvent) {
-        // },
-        // onSearch: function (oEvent) {
-        //     // build filter array
-        //     var aFilter = [];
-        //     var sQuery = oEvent.getParameter("query");
-        //     if (sQuery) {
-        //         aFilter.push(new Filter("email", FilterOperator.Contains, sQuery));
-        //     }
+        deleteClient: function (oEvent) {
+            var oClient = oEvent.getSource().getBindingContext().getObject();
+            var oID = oClient.ID;
+            var oFirstName = oClient.FirstName;
+            var oSecondName = oClient.SecondName;
+            var dialogText = "Do you really want to delete user " + oFirstName + " " + oSecondName + "?";
 
-        //     // filter binding
-        //     var oList = this.byId("ClientsTable");
-        //     var oBinding = oList.getBinding("email");
-        //     oBinding.filter(aFilter);
-        // }
+            //create dialog:
+            this.oResizableDialog = new Dialog({
+                title: "{i18n>confirmAction}",
+                resizable: false,
+                content:
+                    new Text({
+                        text: dialogText,
+                        class: "sapUiSmallMargin"
+                    }),
+                buttons: [
+                    new Button({
+                        text: "{i18n>yes}",
+                        press: function () {
+                            alert("deleting user no. " + oID);
+                            this.oResizableDialog.close();
+                        }.bind(this)
+                    }), new Button({
+                        text: "{i18n>no}",
+                        press: function () {
+                            this.oResizableDialog.close();
+                        }.bind(this)
+                    })]
+            });
+
+            //to get access to the controller's model
+            this.getView().addDependent(this.oResizableDialog);
+
+            this.oResizableDialog.open();
+        }
     });
 });
